374f3d0e7ccb02aa3787bb81663b53bcb50be9d2
bachvu <bach.vu@commissionfactory.com>
2023-12-27 20:51:36 +0700

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,12 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Hello world</Button>
-      </header>
+      <Dashboard />
     </div>
   );
 }
