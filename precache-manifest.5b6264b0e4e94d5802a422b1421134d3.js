87395eafa070c51e1a53e9a42c313d309ff0db5f
bachvu <bach.vu@commissionfactory.com>
2024-01-26 17:57:44 +0700

    Updates

---
+++ precache-manifest.5b6264b0e4e94d5802a422b1421134d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1f96e6936dfbdc42b24f67ed54764f",
+    "url": "/react-github-page-1/index.html"
+  },
+  {
+    "revision": "ed6cabdd6f7301602420",
+    "url": "/react-github-page-1/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8a4ebe99070961b92ad8",
+    "url": "/react-github-page-1/static/js/2.1c71ea68.chunk.js"
+  },
+  {
+    "revision": "de5d16f85c1d84588e52be12a496202f",
+    "url": "/react-github-page-1/static/js/2.1c71ea68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a22c77552644f05c6c7",
+    "url": "/react-github-page-1/static/js/3.6aec4cdf.chunk.js"
+  },
+  {
+    "revision": "ed6cabdd6f7301602420",
+    "url": "/react-github-page-1/static/js/main.4c5cdd17.chunk.js"
+  },
+  {
+    "revision": "f3756971c224cc9e849d",
+    "url": "/react-github-page-1/static/js/runtime-main.228ae074.js"
+  }
+]);
